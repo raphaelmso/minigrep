0db417ed090cc030fc339968c043f99f20626feb
raphaelmso <raphael.msoliveira@gmail.com>
2024-11-18 18:59:52 -0300

    initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn print_arg(&self) {
+        println!("query = {}\nfile_path = {}", self.query, self.file_path);
+    }
+
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap();
+    config.print_arg();
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("{}", contents);
 }
