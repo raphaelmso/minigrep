a2aa4bef02bb679d8d90edbee2d13a9fb87e4592
raphaelmso <raphael.msoliveira@gmail.com>
2024-11-18 21:46:00 -0300

    moved functions to lib.rs and improved error handling

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn print_arg(&self) {
+        println!("query = {}\nfile_path = {}", self.query, self.file_path);
+    }
+
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("{}", contents);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
+use minigrep::run;
+use minigrep::Config;
 use std::env;
-use std::fs;
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn print_arg(&self) {
-        println!("query = {}\nfile_path = {}", self.query, self.file_path);
-    }
-
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
-    }
-}
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     config.print_arg();
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("{}", contents);
+    if let Err(e) = run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    }
 }
