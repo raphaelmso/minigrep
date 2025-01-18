9f2c675e9ef8901b719f0751a3461221c8a05fde
raphaelmso <raphael.msoliveira@gmail.com>
2025-01-17 23:20:48 -0300

    remove logic from main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use minigrep::Config;
+use std::{env, process};
+
 fn main() {
-    println!("Hello, grep!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("m̀inigrep: problem parsing arguments {err}");
+        process::exit(1)
+    });
+
+    println!(
+        "Searching for {}\nInside {}",
+        config.query, config.file_path
+    );
+
+    if let Err(e) = minigrep::run(config) {
+        println!("minigrep: {e}");
+    }
 }
